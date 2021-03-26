cef1185cc45145f0a9dafca918eb552d0534fed1
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2021-03-26 12:29:50 +0200

    ConfirmAccount => hooks

--- src/components/ConfirmAccount/ConfirmAccount.jsx
+++ src/components/ConfirmAccount/ConfirmAccount.jsx
@@ -1,12 +1,11 @@
 import { Form, Formik } from "formik";
 import React from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { removeConfirmToken } from "../../redux/auth/auth.actions";
 import { confirmAccount } from "../../redux/auth/auth.operations";
 import { Modal } from "../../shared/components";
-<<<<<<< HEAD
-import TextInput from "../../shared/components/TextInput";
+// import TextInput from "../../shared/components/TextInput";
 import FormControl from "../FormControl/FormControl";
 import * as Yup from "yup";
 
@@ -15,36 +14,31 @@ const validationSchema = Yup.object().shape({
     .length(6, "min length 6 symbols")
     .required("the field is required"),
 });
-=======
-// import TextInput from "../../shared/components/TextInput";
-import FormControl from "../FormControl/FormControl";
-import * as Yup from "yup"
 
-
-const validationSchema = Yup.object().shape({
-  verificationCode: Yup.string().length(6, "min length 6 symbols").required( "the field is required")
-})
-
->>>>>>> 18ecb14591514472d4757e2be15a0dc09fd3ad78
-
-const ConfirmAccount = ({
-  confirmToken,
-  confirmAccount,
-  removeConfirmToken,
-}) => {
+const ConfirmAccount = () => {
   const history = useHistory();
+
+  const dispatch = useDispatch();
+
+  const confirmToken = useSelector((state) => Boolean(state.auth.confirmToken));
+  const loading = useSelector((state) => state.auth.loading);
+
   return (
-    <Modal open={true} onClose={removeConfirmToken}>
+    <Modal open={confirmToken} onClose={() => dispatch(removeConfirmToken())}>
       <Formik
         validationSchema={validationSchema}
         initialValues={{ verificationCode: "" }}
         onSubmit={({ verificationCode }) => {
-          confirmAccount(verificationCode, history);
+          dispatch(confirmAccount(verificationCode, history));
         }}
       >
         <Form>
           <FormControl type="string" name="verificationCode" />
-          <button type="submit" className="btn btn-primary mt-2 w-100">
+          <button
+            type="submit"
+            className="btn btn-primary mt-2 w-100"
+            disabled={loading}
+          >
             Confirm
           </button>
         </Form>
@@ -53,14 +47,4 @@ const ConfirmAccount = ({
   );
 };
 
-const mapState = (state) => ({
-  confirmToken: Boolean(state.auth.confirmToken),
-  loading: state.auth.loading,
-});
-
-const mapDispatch = {
-  confirmAccount,
-  removeConfirmToken,
-};
-
-export default connect(mapState, mapDispatch)(ConfirmAccount);
+export default ConfirmAccount;
